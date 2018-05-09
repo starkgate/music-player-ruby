73156a25fc8ecbe8f7c9d58c83095e1b8c95e11b
CreeperLava <lavacreeper@hotmail.com>
2018-05-09 14:05:02 +0200

    Minor syntax improvements

--- music.rb
+++ music.rb
@@ -8,54 +8,55 @@ music_dir = "PUT WHATEVER YOU LIKE HERE"
 # in case you only want specific folders to be scanned in your music directory
 music_folders = ""
 
-options = {:name => "", :type => 'd', :rand => false}
+options = { name: '', type: 'd', rand: false }
 OptionParser.new do |opts|
-  opts.banner = "Usage: music [options]"
+  opts.banner = 'Usage: music [options]'
 
-  opts.on("-nNAME", "--name NAME", "Search for keywords") do |name|
+  opts.on('-nNAME', '--name NAME', 'Search for keywords') do |name|
     options[:name] = name
   end
 
-  opts.on("-f", "--file", "Search for files (default folders)") do |name|
+  opts.on('-f', '--file', 'Search for files (default folders)') do |name|
     options[:type] = 'f'
   end
 
-  opts.on("-r", "--rand", "Shuffle the playlist") do |name|
+  opts.on('-r', '--rand', 'Shuffle the playlist') do |name|
     options[:rand] = true
   end
 
-  opts.on("-h", "--help", "Prints this help") do
+  opts.on('-h', '--help', 'Prints this help') do
     puts opts
     exit
   end
 end.parse!
 
 Dir.chdir music_dir
-search = `find #{music_folders} -type #{options[:type]} -iname '*#{options[:name].gsub(/\s/, "*")}*'`
-if search == ""
-  puts "[MUSIC][SEARCH] Nothing found, try searching for something else"
+search = `find #{music_folders} -type #{options[:type]} -iname '*#{options[:name].gsub(/\s/, '*')}*'`
+if search == ''
+  puts '[MUSIC][SEARCH] Nothing found, try searching for something else'
   return
 else
   puts "[MUSIC][SEARCH] Found'em ! Queuing..."
 end
 
 @songs = search.split(/\n/)
-if options[:type] == "d"
-  if options[:name] == ""
+if options[:type] == 'd'
+  if options[:name] == ''
     play = music_folders # entire library
   else
     play = @songs[0]
   end
   @songs = `find '#{play}' -type f -iname '*.mp3' -o -iname '*.flac'`.split(/\n/)
 end
-puts "[MUSIC][PLAY] Playing the following #{@songs.length} songs in #{options[:rand] ? 'shuffle' : 'sequential'} mode"
+length = @songs.length
+puts "[MUSIC][PLAY] Playing the following #{length} songs in #{options[:rand] ? 'shuffle' : 'sequential'} mode"
 
 @songs.shuffle! if options[:rand]
-@song_names = @songs.map{|song| "\t" + song.gsub(/^.*\//, "") + "\n\r"}
+@song_names = @songs.map { |song| "\t" + song.gsub(/^.*\//, '') + "\n\r" }
 
 run = true
 neg = 1
-@cmd = ["STOP", "CONT"]
+@cmd = %w[STOP CONT]
 @toggle = 0
 
 def rotateSongs n
@@ -73,7 +74,7 @@ def prevSong
 end
 
 def jumpSong x
-  rotateSongs (x-1)
+  rotateSongs (x - 1)
   nextSong
 end
 
@@ -83,19 +84,19 @@ def pauseSong
 end
 
 @user_input = Thread.new do
-  while(true)
+  loop do
     input = STDIN.getch
-    if input == "\e" then
+    if input == "\e"
       input << STDIN.read_nonblock(3) rescue nil
       input << STDIN.read_nonblock(2) rescue nil
     end
 
     case input
-    when " "
-	  pauseSong
-    when "-"
+    when ' '
+      pauseSong
+    when '-'
       neg *= -1
-    when "\e[B", "\e[C", 'n', "1"
+    when "\e[B", "\e[C", 'n', '1'
       nextSong
     when "\e[A", "\e[D", 'p'
       prevSong
@@ -108,29 +109,29 @@ end
       pauseSong if @toggle == 1
       system("kill #{`pidof play`}")
       Thread.exit
-    when "2","3","4","5","6","7","8","9"
-      jumpSong (input.to_i*neg)
-    STDIN.echo = false
-    sleep 0.2
-    STDIN.echo = true
+    when '2', '3', '4', '5', '6', '7', '8', '9'
+      jumpSong (input.to_i * neg)
+      STDIN.echo = false
+      sleep 0.2
+      STDIN.echo = true
     end
   end
 end
 
-while(run)
-  system("clear")
+while run
+  system('clear')
   printf "[MUSIC][PLAY] Press h to see the keyboard shortcuts\n\r"
   song_length = `soxi -d "#{@songs[0]}"`.slice(3..-5).chomp
   printf "Playing #{@song_names[0].lstrip.chomp.chomp} (#{song_length}) of #{length} songs\n\r\n\r\n\r"
 
   # print only the surrounding 25 songs at most
   prev = @song_names[-12..-1]
-  printf prev.join if prev != nil
+  printf prev.join unless prev.nil?
   print "[PLAY]#{@song_names[0]}"
   printf @song_names[1..12].join # remove leading path, keep only filenames of songs
 
   @current_song = Thread.new do
-  	str = '"' + @songs[0] + '"' # sanitize string
+    str = '"' + @songs[0] + '"' # sanitize string
     system("play -V1 -q #{str}")
   end
   @current_song.join
